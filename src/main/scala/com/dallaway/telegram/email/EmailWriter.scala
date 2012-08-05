7ce9e1ff1104e6511e3e4443199bd0673a7e00c0
Richard Dallaway <richard@dallaway.com>
2012-08-05 17:42:21 +0100

    Can now handle in-line images.

--- src/main/scala/com/dallaway/telegram/email/EmailWriter.scala
+++ src/main/scala/com/dallaway/telegram/email/EmailWriter.scala
@@ -4,10 +4,11 @@ import scalax.io._
 import Resource._
 import scalax.file.Path
 import scalax.file.PathMatcher._
-
 import javax.mail._
 import javax.mail.internet._
 import com.sun.mail.imap._
+import java.io.FilterInputStream
+import java.io.InputStream
 
 
 trait EmailWriter {
@@ -20,7 +21,7 @@ trait EmailWriter {
 			m.getSubject,
 			body(m),
 			m.getSentDate,
-			for ( a ← attachments(m); sa ← savedAttachment(mediaDir, a) ) yield sa )
+			for ( a ← attachments(mediaDir, m) ) yield a )
 
 
   implicit def pathHelper(path: Path) = new {
@@ -43,7 +44,7 @@ trait EmailWriter {
 	}
 
 	// "IMAGE/JPEG; name=1557861979_af07a31642.jpg" ⇒ "image/jpg"
-	implicit def bodyHelper(b: BodyPart) = new {
+	implicit def bodyHelper(b: Part) = new {
 		def mimeType = b.getContentType.split(";").head.toLowerCase
 	}    
 
@@ -53,28 +54,34 @@ trait EmailWriter {
 		case _ ⇒ ""
 	}
 
-	private def attachments(m: Part) : Seq[BodyPart] = m.getContent match {
-		case p: MimeMultipart => for {
+	private def attachments(mediaDir: Path, m: Part) : Seq[Attachment] = m.getContent match {
+		case p: MimeMultipart ⇒ for {
 			i ← 0 until p.getCount
 			b = p.getBodyPart(i)
 			if b.getDisposition != null
-		} yield b
-		case _ ⇒ Nil 
+			a ← savedAttachment(mediaDir, b.getInputStream, b.mimeType, b.getFileName)
+		} yield a
+		
+		
+		// Inline content
+		case s: InputStream ⇒	savedAttachment(mediaDir, s, m.mimeType, m.getFileName).toList
+			
+		case otherwise ⇒ println("Found a "+otherwise); Nil 
 	}
 
-	private def savedAttachment(mediaDir: Path, b: BodyPart) : Option[Attachment] = {
+	private def savedAttachment(mediaDir: Path, in: ⇒InputStream, mimeType: String, fileName: String) : Option[Attachment] = {
 		// Save original image to link to:
-		val dest = (mediaDir / b.getFileName).ensureUnique
-		dest.write(fromInputStream(b.getInputStream).bytes)
+		val dest = (mediaDir / fileName).ensureUnique
+		dest.write(fromInputStream(in).bytes)
 
 		// Scaled version to show in the blog:
 		val width = 500
 		val inlineImgName = dest.name + "."+width+"." + dest.name.split("\\.").last
 		val inlineFile = mediaDir / inlineImgName
 
-		for ( inlineSize ← ImageResizer.scale(dest, b.mimeType, inlineFile, width) ) 
+			for ( inlineSize ← ImageResizer.scale(dest, mimeType, inlineFile, width) ) 
 		  yield 
-		  	ImageAttachment("/media/"+b.getFileName, "/media/"+inlineImgName, inlineSize, b.mimeType)
+		  	ImageAttachment("/media/"+dest.name, "/media/"+inlineImgName, inlineSize, mimeType)
 
 	}
 
